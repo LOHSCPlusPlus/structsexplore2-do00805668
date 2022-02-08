079b619314f4905aa6410f7a7d5ce37b95369db8
AUGUSTINEDO <>
2022-02-08 17:22:47 +0000

    Explore10 working

--- Explore10.cpp
+++ Explore10.cpp
@@ -6,14 +6,19 @@ struct Broken {
     Broken(int initialValues[4]);
 };
 Broken::Broken() {
-    // int does not have a default constructor
-    // modify this code so that all elements in values
-    // are set to 0.
+    for(int i = 0; i<4; i++)
+    {
+      values[i] = 0;
+    }
 }
 Broken::Broken(int initialValues[4]) {
     // Change only this code so that values will hold the
     // values in initialValues
-    values = initialValues;
+    //values = initialValues;
+    for(int i = 0; i < 4; i++)
+    {
+      values[i] = initialValues[i];
+    }
 }
 
 void PrintBroken(Broken x) {
